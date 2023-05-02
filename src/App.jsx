99029d16b4b346b023d2ef127002e188141bacd8
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-05-02 09:13:12 +0600

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Components/Header/Navbar'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -7,7 +8,10 @@ function App() {
   return (
     <div className='container mx-auto'>
       <Navbar />
+      <div className='min-h-[calc(100vh-140px)]'>
       <Outlet />
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-purple-200 p-5 text-center'>
+            <p><span>&copy;</span> Copyright 2023 by <span className='font-bold'>Recipe World.</span> All Rights Reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
