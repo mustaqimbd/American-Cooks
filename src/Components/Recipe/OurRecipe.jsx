9d5f449a63f19e902f61383edc2ee8ef9932bcce
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-05-03 00:14:18 +0600

    Recipe gallery section added

--- src/Components/Recipe/OurRecipe.jsx
+++ src/Components/Recipe/OurRecipe.jsx
@@ -1,7 +1,48 @@
 
 const OurRecipe = () => {
     return (
-       
+        <div>
+            <h1 className="text-center text-3xl font-bold mt-10 mb-6">Our Best Easy Healthy Dinner Recipes gallery</h1>
+            <div className="grid grid-cols-3 gap-4 w-[80%] mx-auto">
+                <div>
+                    <img className="w-full h-[150px]" src="https://images.immediate.co.uk/production/volatile/sites/30/2012/09/Beef-wellington-d4f3320.jpg?quality=90&webp=true&resize=300,272" alt="" />
+                    <p className="text-center mt-2">Beef wellington</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://cdn.loveandlemons.com/wp-content/uploads/2021/05/scrambled-eggs-1-580x851.jpg" alt="" />
+                    <p className="text-center mt-2">Scrambled Eggs</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://cafedelites.com/wp-content/uploads/2018/05/Jambalaya-IMAGE-54-1365x2048.jpg" alt="" />
+                    <p className="text-center mt-2">roasted lemon chicken</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://images.immediate.co.uk/production/volatile/sites/30/2012/09/Beef-wellington-d4f3320.jpg?quality=90&webp=true&resize=300,272" alt="" />
+                    <p className="text-center mt-2">Beef wellington</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://www.thespruceeats.com/thmb/2VjmwngG_vwtAWbGRcTLKeihEZk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/connecticut-style-lobster-roll-recipe-5496594-hero-05-26128df34866403997c3b15cb345f18d.jpg" alt="" />
+                    <p className="text-center mt-2">Lobster Rolls</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://www.foodandwine.com/thmb/ZRQWVkrrpkwypbVdM6irFMycfkc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/200804-r-herb-roasted-chicken-d02d649f6e60441b8baf0bb5ef28fcdf.jpg" alt="" />
+                    <p className="text-center mt-2">roasted lemon chicken</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://www.foodandwine.com/thmb/ZRQWVkrrpkwypbVdM6irFMycfkc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/200804-r-herb-roasted-chicken-d02d649f6e60441b8baf0bb5ef28fcdf.jpg" alt="" />
+                    <p className="text-center mt-2">roasted lemon chicken</p>
+                </div>
+                <div>
+                    <img className="w-full h-[150px]" src="https://cdn.loveandlemons.com/wp-content/uploads/2021/05/scrambled-eggs-1-580x851.jpg" alt="" />
+                    <p className="text-center mt-2">Scrambled Eggs</p>
+                </div>
+
+                <div>
+                    <img className="w-full h-[150px]" src="https://images.immediate.co.uk/production/volatile/sites/30/2012/09/Beef-wellington-d4f3320.jpg?quality=90&webp=true&resize=300,272" alt="" />
+                    <p className="text-center mt-2">Beef wellington</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Banner from '../Components/Header/Banner';
 import Chefs from '../Components/Chefs/Chefs';
 import { useLoaderData } from 'react-router-dom';
+import OurRecipe from '../Components/Recipe/OurRecipe';
 
 const Home = () => {
-    
+
     return (
         <div>
             <Banner />
             <Chefs />
+            <OurRecipe />
         </div>
     );
 };
