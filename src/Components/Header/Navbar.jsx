104245b6cb3912f66201c127c2b75d6643e598ee
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-05-03 14:58:56 +0600

    spinner added

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../Provider/AuthProvider';
@@ -20,6 +19,20 @@ const Navbar = () => {
                 <li><Link>Blog</Link></li>
             </ul>
             <div>
+                {loading && <div aria-label="Loading..." role="status" className="flex items-center space-x-2">
+                    <svg className="h-10 w-10 mr-8 animate-spin stroke-gray-500" viewBox="0 0 256 256">
+                        <line x1="128" y1="32" x2="128" y2="64" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="224" y1="128" x2="192" y2="128" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="128" y1="224" x2="128" y2="192" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="32" y1="128" x2="64" y2="128" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                        <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
+                    </svg>
+                    
+                </div>
+                }
                 {
                     user ? <div className=' flex gap-4'><img className='w-10 h-10 rounded-full' src={user.photoURL && user.photoURL} alt="img" title={user.displayName && user.displayName} /> <button className='py-2 px-3 text-lg font-bold bg-purple-700 rounded-md text-white' onClick={logOUt}>Log out</button></div>
                         : loading == false && <span><Link to='/login' className='py-2 px-3 text-lg font-bold bg-purple-700 rounded-md text-white'>Login</Link></span>
